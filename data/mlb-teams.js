a9f38269050e254303d14921955b6e7f7c075116
Alejandro Bay <alejandrobay23@gmail.com>
2022-11-08 02:59:25 -0500

    this is the first commit

---
+++ data/mlb-teams.js
@@ -0,0 +1,28 @@
+const teams = {
+  yankees: {
+    'Player 1': 'Aaron Judge',
+    'Player 2': 'Gleiber Torres',
+    'Player 3': 'Giancarlo Stanton',
+  },
+  'blue jays': {
+    'Player 1': 'Teoscar Hernandez',
+    'Player 2': 'Alejandro Kirk',
+    'Player 3': 'Bo Bichette',
+  },
+  rays: {
+    'Player 1': 'Yandy Diaz',
+    'Player 2': 'Wander Franco',
+    'Player 3': 'Jose Siri',
+  },
+  orioles: {
+    'Player 1': 'Cedric Mullins',
+    'Player 2': 'Anthony Santander',
+    'Player 3': 'Ryan Mountcastle',
+  },
+  unknown: {
+    'Player 1': 'unknown',
+    'Player 2': 'unknownr',
+    'Player 3': 'unknown',
+  },
+}
+module.exports = teams
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const teams = require('./data/mlb-teams')
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+  const teamName = req.params.name.toLowerCase()
+  if (teams[teamName]) {
+    res.json(teams[teamName])
+  } else {
+    res.json(teams['unknown'])
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}! Let's Gooo...`)
+})
